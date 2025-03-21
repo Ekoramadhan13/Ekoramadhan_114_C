93b274c87619c157e459f28b42a550022c10acd0
Ekoramadhan13 <enugraha411@gmail.com>
2025-03-21 18:43:17 +0700

    Menyimpan koneksi ke database SQL Server dengan database OrganisasiMahasiswa1.

--- LatihanCRUD/Form1.cs
+++ LatihanCRUD/Form1.cs
@@ -11,5 +11,29 @@ using System.Windows.Forms;
 
 namespace LatihanCRUD
 {
-   
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=LAPTOP-E6D6ULI6\\EKORAMADHAN;Initial Catalog=OrganisasiMahasiswa1;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void ClearForm()
+        {
+            txtNIM.Clear();
+            txtNama.Clear();
+            txtEmail.Clear();
+            txtTelepon.Clear();
+            txtAlamat.Clear();
+            txtNIM.Focus();  // Fokus kembali ke NIM agar user siap memasukkan data baru
+        }
+
+    }
 }
